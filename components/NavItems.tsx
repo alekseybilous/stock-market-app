e978efe682ebb0c03317267b456a33d566163d74
Oleksii Bilous <oleksii.bilous@ext.hellofresh.com>
2025-09-30 16:23:20 +0300

    fix comments

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -23,7 +23,7 @@ const NavItems = ({ initialStocks }: NavItemsProps) => {
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
       {NAV_ITEMS.map(({ href, label }) => {
-        if (label === "Search") {
+        if (href === "/search") {
           return (
             <li key="search-trigger">
               <SearchCommand
